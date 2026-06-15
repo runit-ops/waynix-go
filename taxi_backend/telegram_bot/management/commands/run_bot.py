e8aae8601f203a02cce509db8ea82229db2347a9
ruslan78867886 <ruslan78867886@gmail.com>
2026-06-15 17:21:57 +0500

    Remove backend from frontend branch

--- taxi_backend/telegram_bot/management/commands/run_bot.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    help = "Запуск Telegram-бота"
-    def handle(self, *args, **options):
-        from telegram_bot.bot import run_bot
-        run_bot()
