ecde52342558d1acba6b679e221ae86ece0c755c
ruslan78867886 <ruslan78867886@gmail.com>
2026-06-15 17:29:38 +0500

    start.sh: --noreload for production

--- taxi_backend/start.sh
+++ taxi_backend/start.sh
@@ -2,6 +2,6 @@
 cd "$(dirname "$0")"
 [ -d "venv" ] && source venv/bin/activate
 [ -f ".env" ] && export $(grep -v '^#' .env | xargs)
-python manage.py runserver 0.0.0.0:8000 &
+python manage.py runserver 0.0.0.0:8000 --noreload &
 python manage.py run_bot &
 wait
